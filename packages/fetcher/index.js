7137f03cc7b46c1d004a8f9ec9e3a96d9f8f3c8f
Soheil <soheilzarepoor79@gmail.com>
2024-07-15 14:09:41 +0330

    feat: add fetcher service

---
+++ packages/fetcher/index.js
@@ -0,0 +1,99 @@
+import axios from "axios";
+import Redis from "ioredis";
+import cron from "node-cron";
+
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || "6379", 10),
+});
+
+cron.schedule("*/20 * * * *", () => {
+    console.log("Running scheduled task...");
+    getAllCheapest();
+});
+
+export async function getAllCheapest() {
+    try {
+        await getCheapest("1");
+        await new Promise((resolve) => setTimeout(resolve, 60000));
+        await getCheapest("3");
+        await new Promise((resolve) => setTimeout(resolve, 60000));
+        await getCheapest("6");
+        await new Promise((resolve) => setTimeout(resolve, 60000));
+        await getCheapest("12");
+    } catch (error) {}
+}
+
+async function getCheapest(month) {
+    const countriesList = await getCountriesList();
+    const prices = [];
+
+    console.log(`fetching for ${month} month`);
+
+    for (const countryCode of countriesList) {
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        const price = await getPrice(countryCode, month, "LTC");
+        prices.push({ countryCode, price });
+    }
+
+    const sanitizedPrices = prices.filter((price) => price.price > 0);
+
+    const cheapest = sanitizedPrices.sort((a, b) => a.price - b.price)[0];
+    const coinPrice = await getCoinPrice("LTC");
+    cheapest.price = "$" + (coinPrice * cheapest.price).toFixed(2);
+
+    console.log(cheapest);
+
+    await redis.set(`cheapest-${month}`, JSON.stringify(cheapest));
+}
+
+async function getCountriesList() {
+    try {
+        const { data } = await axios.get(
+            "https://backend.coinsbee.com/api/v1/brands/info/Spotify/en"
+        );
+
+        return data.countries;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+async function getPrice(countryCode, month, coin) {
+    try {
+        const { data } = await axios.get(
+            `https://backend.coinsbee.com/api/v1/products/list/Spotify/${countryCode}/en/
+            `
+        );
+        console.log("requesting");
+
+        const result = data.results.find((result) =>
+            result.name.startsWith(`Spotify ${month} Month`)
+        );
+        if (result) {
+            const { id, value } = result;
+
+            const { data: priceRequestData } =
+                await axios.get(`https://backend.coinsbee.com/api/v1/cart/price/${id}}/${value}/${coin}
+            `);
+
+            const newPrice = priceRequestData.results.supplier.price;
+
+            return newPrice;
+        }
+        return "-1";
+    } catch (error) {
+        console.log(error);
+        return "-1";
+    }
+}
+
+async function getCoinPrice(coin) {
+    const { data } = await axios.get(
+        `https://open-api.bingx.com/openApi/swap/v2/quote/ticker?symbol=${coin}-USDT`
+    );
+
+    const newPrice = data.data.lastPrice;
+    return newPrice;
+}
